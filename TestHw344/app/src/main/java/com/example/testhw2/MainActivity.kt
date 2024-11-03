903d0326cae2fdc12079f70992ad7af952237858
Adiletkcid <Adiletkcid@users.noreply.github.com>
2024-11-03 13:43:06 +0500

    LazyGrid

--- TestHw344/app/src/main/java/com/example/testhw2/MainActivity.kt
+++ TestHw344/app/src/main/java/com/example/testhw2/MainActivity.kt
@@ -7,13 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.testhw2.movieItems.VerticalGridScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                NavigationToFirstPage()
+                VerticalGridScreen()
             }
         }
     }
